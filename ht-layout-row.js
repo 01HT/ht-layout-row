984927efc29882cf48d37ecb94a05430ac8f8f2d
Maksim Yurkov <yurkov.maksim.sergeevich@gmail.com>
2018-03-10 19:56:50 +0300

    Update ht-layout-row.js

--- ht-layout-row.js
+++ ht-layout-row.js
@@ -1,5 +1,5 @@
 "use strict";
-import { LitElement, html } from "../@polymer/lit-element/lit-element.js";
+import { LitElement, html } from "@polymer/lit-element";
 
 class HTLayoutRow extends LitElement {
   render({ card }) {
